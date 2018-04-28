b2290b63100cf35d0d6ae0f4b6a8a6d835b4264d
onionzhou <892723057@qq.com>
2018-04-28 16:10:12 +0800

    web login test ok

--- test/test_login.py
+++ test/test_login.py
@@ -3,27 +3,30 @@
 #
 #6.0 web login
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 import time
 
 def login():
     driver = webdriver.Chrome()
     driver.get("https://cloud.idste.org/")
+    wait = WebDriverWait(driver,4) #显示等待4s
+    driver.maximize_window()
     time.sleep(1)
     #driver.find_element_by_xpath("//input[@type='phone']").send_keys('13982004967')
     driver.find_element_by_class_name("el-input__inner").send_keys("13982004967")
-    time.sleep(2)
-   # driver.find_elements_by_css_selector("[type = 'button']")
+
     driver.find_element_by_class_name("login-btn").click()
-   # driver.find_element_by_xpath("//button[@type='button']").click()
-    #driver.find_element_by_link_text(u"登录").click()
-    time.sleep(10)
-
-
-
-
-
-
-
+    time.sleep(1)
+    driver.find_element(By.CLASS_NAME,'school-name').click()
+    time.sleep(1)
+    wait.until(lambda test:test.find_element(By.CLASS_NAME,'el-input__inner'))
+    #driver.find_element(By.CLASS_NAME,'el-input__inner').send_keys("onion")
+    driver.find_element_by_xpath("//input[@type='password']").send_keys("onion")
+    #driver.find_element(By.CLASS_NAME,'el-icon-back').click()
+    driver.find_element(By.XPATH,"//i[@class='el-icon-back']").click()
 
 if __name__ == '__main__':
+
     login()
