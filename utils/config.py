9f828e8fc08b89a9149f8fce0e0ff16944876541
onionzhou <892723057@qq.com>
2018-05-02 18:48:19 +0800

    fix log file should in log dir

--- utils/config.py
+++ utils/config.py
@@ -5,7 +5,7 @@ BASE_PATH = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
 CONFIG_FILE = os.path.join(BASE_PATH,'config','config.yaml')
 DATA_PATH = os.path.join(BASE_PATH,'data')
 DRIVER_PATH = os.path.join(BASE_PATH, 'drivers')
-LOG_PATH = os.path.join(BASE_PATH, 'log')
+LOG_PATH = os.path.join(BASE_PATH, 'log')+ '\ '
 REPORT_PATH = os.path.join(BASE_PATH, 'report')
 
 class Config():
@@ -20,4 +20,5 @@ if __name__ == '__main__':
     url = Config().get('TEST4')
     print(url)
     print(DRIVER_PATH +'\cc')
+    print(LOG_PATH)
 
