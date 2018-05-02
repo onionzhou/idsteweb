518f5387efd995184f4e2705ea5f8a27ca4a5ef8
onionzhou <892723057@qq.com>
2018-05-02 19:22:01 +0800

    suport excel file read but is undone

--- utils/fileRead.py
+++ utils/fileRead.py
@@ -1,6 +1,6 @@
 import yaml
 import os
-
+import xlrd
 class YamlRead():
     def __init__(self,yaml_file):
         if os.path.exists(yaml_file):
@@ -14,3 +14,21 @@ class YamlRead():
             with open(self.yaml_file,'rb') as f:
                self._data = list(yaml.safe_load_all(f))
         return  self._data
+
+class ExcelRead():
+    '''read excel file
+        return list
+        | A| B| C|
+        |a1|b1|c1|
+        |a2|b2|c2|
+        '''
+    def __init__(self,execlfile,sheet,title_line):
+        if os.path.exists(execlfile):
+            self.execl =execlfile
+        else:
+            raise FileNotFoundError('file is not exist')
+        self.sheet =sheet
+        self.title_line = title_line
+        self._data = list()
+
+
